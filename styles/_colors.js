60d2621018d2e189e71bf821e2e41acebf365065
Przemysław Stochel <przemyslaw.stochel@revolve.pro>
2022-12-13 10:29:11 +0100

    YLH-41 tsconfig added

---
+++ styles/_colors.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var colors = {
+    blue100: '#305971',
+    blue90: '#356781',
+    blue80: '#4c788f',
+    blue70: '#62899d',
+    blue60: '#799aab',
+    blue50: '#8faab9',
+    blue40: '#a5bbc7',
+    blue30: '#bcccd5',
+    blue20: '#d2dde3',
+    blue10: '#e9eef1',
+    blue7: '#f4f7f8',
+    offBlack: '#1c1d1d',
+    offWhite: '#fefefe',
+    grey2: '#809294',
+    grey3: '#555555',
+    greyLightM: '#e0e0e0',
+    greyLightL: '#d0d1d2',
+    greyMedWcag: '#4e595a',
+    mintLightMint: '#f6fbfb',
+    mint3Light: '#fafdfd',
+    mintVeryLight: '#fafdfd',
+    mint4: '#f4fafa',
+    orange: '#ffc857',
+    orangeLight: '#fff4dd',
+    red: '#e5323b',
+    redLight: '#fef5f5',
+    mdsLightBeerus: '#EBEBEB',
+};
+exports.default = colors;
