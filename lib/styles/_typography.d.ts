d8f74d1969c4e1fc00810f8de18c5b2e3b7b10f6
pstochel-revolve <22317677+100hel@users.noreply.github.com>
2022-12-13 14:51:49 +0100

    Feature/YLH-41 Creating a separate repository for the typography and images (#1)
    * YLH-41 sass added to package.json
    
    * YLH-41 colors and typography added
    
    * YLH-41 colors and typography showcase
    
    * YLH-41 css in js added
    
    * YLH-41 tsconfig added
    
    * YLH-41 Updating resources section added to readme
    
    * YLH-41 resources moved to lib folder

---
+++ lib/styles/_typography.d.ts
@@ -0,0 +1,191 @@
+declare const typography: {
+    readonly DM_Med_8_8: {
+        readonly fontSize: "8px";
+        readonly fontWeight: 500;
+        readonly lineHeight: "8px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Reg_10_10: {
+        readonly fontSize: "10px";
+        readonly fontWeight: 400;
+        readonly lineHeight: "10px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Med_10_12: {
+        readonly fontSize: "10px";
+        readonly fontWeight: 500;
+        readonly lineHeight: "12px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Reg_12_12_Chip: {
+        readonly fontSize: "12px";
+        readonly fontWeight: 400;
+        readonly lineHeight: "12px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Med_12_12_Chip: {
+        readonly fontSize: "12px";
+        readonly fontWeight: 500;
+        readonly lineHeight: "12px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Reg_12_16: {
+        readonly fontSize: "12px";
+        readonly fontWeight: 400;
+        readonly lineHeight: "16px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Med_12_16: {
+        readonly fontSize: "12px";
+        readonly fontWeight: 500;
+        readonly lineHeight: "16px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Reg_14_32_Table: {
+        readonly fontSize: "14px";
+        readonly fontWeight: 400;
+        readonly lineHeight: "32px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Med_14_20: {
+        readonly fontSize: "14px";
+        readonly fontWeight: 500;
+        readonly lineHeight: "20px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Med_14_16: {
+        readonly fontSize: "14px";
+        readonly fontWeight: 500;
+        readonly lineHeight: "16px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Reg_14_16: {
+        readonly fontSize: "14px";
+        readonly fontWeight: 400;
+        readonly lineHeight: "16px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Reg_14_24: {
+        readonly fontSize: "14px";
+        readonly fontWeight: 400;
+        readonly lineHeight: "24px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Med_16_16: {
+        readonly fontSize: "16px";
+        readonly fontWeight: 500;
+        readonly lineHeight: "16px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Btn_16_16_Med: {
+        readonly fontSize: "16px";
+        readonly fontWeight: 500;
+        readonly lineHeight: "16px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Reg_16_16_Chip: {
+        readonly fontSize: "16px";
+        readonly fontWeight: 400;
+        readonly lineHeight: "16px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Reg_16_20: {
+        readonly fontSize: "16px";
+        readonly fontWeight: 400;
+        readonly lineHeight: "20px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Reg_16_24: {
+        readonly fontSize: "16px";
+        readonly fontWeight: 400;
+        readonly lineHeight: "24px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Med_16_24: {
+        readonly fontSize: "16px";
+        readonly fontWeight: 500;
+        readonly lineHeight: "24px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Body_Reg_16: {
+        readonly fontSize: "16px";
+        readonly fontWeight: 400;
+        readonly lineHeight: "32px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Bold_16_24: {
+        readonly fontSize: "16px";
+        readonly fontWeight: 700;
+        readonly lineHeight: "24px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Reg_16_24_Italic: {
+        readonly fontSize: "16px";
+        readonly fontStyle: "italic";
+        readonly fontWeight: 400;
+        readonly lineHeight: "24px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Med_18_24: {
+        readonly fontSize: "18px";
+        readonly fontWeight: 500;
+        readonly lineHeight: "24px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Med_20_24: {
+        readonly fontSize: "20px";
+        readonly fontWeight: 500;
+        readonly lineHeight: "24px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Med_24_24: {
+        readonly fontSize: "24px";
+        readonly fontWeight: 500;
+        readonly lineHeight: "24px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Med_32_32: {
+        readonly fontSize: "32px";
+        readonly fontWeight: 500;
+        readonly lineHeight: "32px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Reg_32_32: {
+        readonly fontSize: "32px";
+        readonly fontWeight: 400;
+        readonly lineHeight: "32px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Med_32_48: {
+        readonly fontSize: "32px";
+        readonly fontWeight: 500;
+        readonly lineHeight: "48px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Reg_40_56: {
+        readonly fontSize: "40px";
+        readonly fontWeight: 400;
+        readonly lineHeight: "56px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Bold_40_56: {
+        readonly fontSize: "40px";
+        readonly fontWeight: 700;
+        readonly lineHeight: "56px";
+        readonly letterSpacing: 0;
+    };
+    readonly DM_Bold_14_24_UC: {
+        readonly fontSize: "14px";
+        readonly fontWeight: 700;
+        readonly lineHeight: "24px";
+        readonly letterSpacing: "0.03em";
+        readonly textTransform: "uppercase";
+    };
+    readonly DM_Med_14_24: {
+        readonly fontSize: "14px";
+        readonly fontWeight: 500;
+        readonly lineHeight: "24px";
+        readonly letterSpacing: 0;
+    };
+};
+export default typography;
---
+++ lib/styles/_typography.ts
@@ -0,0 +1,222 @@
+const typography = {
+  DM_Med_8_8: {
+    fontSize: '8px',
+    fontWeight: 500,
+    lineHeight: '8px',
+    letterSpacing: 0,
+  },
+
+  DM_Reg_10_10: {
+    fontSize: '10px',
+    fontWeight: 400,
+    lineHeight: '10px',
+    letterSpacing: 0,
+  },
+
+  DM_Med_10_12: {
+    fontSize: '10px',
+    fontWeight: 500,
+    lineHeight: '12px',
+    letterSpacing: 0,
+  },
+
+  DM_Reg_12_12_Chip: {
+    fontSize: '12px',
+    fontWeight: 400,
+    lineHeight: '12px',
+    letterSpacing: 0,
+  },
+
+  DM_Med_12_12_Chip: {
+    fontSize: '12px',
+    fontWeight: 500,
+    lineHeight: '12px',
+    letterSpacing: 0,
+  },
+
+  DM_Reg_12_16: {
+    fontSize: '12px',
+    fontWeight: 400,
+    lineHeight: '16px',
+    letterSpacing: 0,
+  },
+
+  DM_Med_12_16: {
+    fontSize: '12px',
+    fontWeight: 500,
+    lineHeight: '16px',
+    letterSpacing: 0,
+  },
+
+  DM_Reg_14_32_Table: {
+    fontSize: '14px',
+    fontWeight: 400,
+    lineHeight: '32px',
+    letterSpacing: 0,
+  },
+
+  DM_Med_14_20: {
+    fontSize: '14px',
+    fontWeight: 500,
+    lineHeight: '20px',
+    letterSpacing: 0,
+  },
+
+  DM_Med_14_16: {
+    fontSize: '14px',
+    fontWeight: 500,
+    lineHeight: '16px',
+    letterSpacing: 0,
+  },
+
+  DM_Reg_14_16: {
+    fontSize: '14px',
+    fontWeight: 400,
+    lineHeight: '16px',
+    letterSpacing: 0,
+  },
+
+  DM_Reg_14_24: {
+    fontSize: '14px',
+    fontWeight: 400,
+    lineHeight: '24px',
+    letterSpacing: 0,
+  },
+
+  DM_Med_16_16: {
+    fontSize: '16px',
+    fontWeight: 500,
+    lineHeight: '16px',
+    letterSpacing: 0,
+  },
+
+  DM_Btn_16_16_Med: {
+    fontSize: '16px',
+    fontWeight: 500,
+    lineHeight: '16px',
+    letterSpacing: 0,
+  },
+
+  DM_Reg_16_16_Chip: {
+    fontSize: '16px',
+    fontWeight: 400,
+    lineHeight: '16px',
+    letterSpacing: 0,
+  },
+
+  DM_Reg_16_20: {
+    fontSize: '16px',
+    fontWeight: 400,
+    lineHeight: '20px',
+    letterSpacing: 0,
+  },
+
+  DM_Reg_16_24: {
+    fontSize: '16px',
+    fontWeight: 400,
+    lineHeight: '24px',
+    letterSpacing: 0,
+  },
+
+  DM_Med_16_24: {
+    fontSize: '16px',
+    fontWeight: 500,
+    lineHeight: '24px',
+    letterSpacing: 0,
+  },
+
+  DM_Body_Reg_16: {
+    fontSize: '16px',
+    fontWeight: 400,
+    lineHeight: '32px',
+    letterSpacing: 0,
+  },
+
+  DM_Bold_16_24: {
+    fontSize: '16px',
+    fontWeight: 700,
+    lineHeight: '24px',
+    letterSpacing: 0,
+  },
+
+  DM_Reg_16_24_Italic: {
+    fontSize: '16px',
+    fontStyle: 'italic',
+    fontWeight: 400,
+    lineHeight: '24px',
+    letterSpacing: 0,
+  },
+
+  DM_Med_18_24: {
+    fontSize: '18px',
+    fontWeight: 500,
+    lineHeight: '24px',
+    letterSpacing: 0,
+  },
+
+  DM_Med_20_24: {
+    fontSize: '20px',
+    fontWeight: 500,
+    lineHeight: '24px',
+    letterSpacing: 0,
+  },
+
+  DM_Med_24_24: {
+    fontSize: '24px',
+    fontWeight: 500,
+    lineHeight: '24px',
+    letterSpacing: 0,
+  },
+
+  DM_Med_32_32: {
+    fontSize: '32px',
+    fontWeight: 500,
+    lineHeight: '32px',
+    letterSpacing: 0,
+  },
+
+  DM_Reg_32_32: {
+    fontSize: '32px',
+    fontWeight: 400,
+    lineHeight: '32px',
+    letterSpacing: 0,
+  },
+
+  DM_Med_32_48: {
+    fontSize: '32px',
+    fontWeight: 500,
+    lineHeight: '48px',
+    letterSpacing: 0,
+  },
+
+  DM_Reg_40_56: {
+    fontSize: '40px',
+    fontWeight: 400,
+    lineHeight: '56px',
+    letterSpacing: 0,
+  },
+
+  DM_Bold_40_56: {
+    fontSize: '40px',
+    fontWeight: 700,
+    lineHeight: '56px',
+    letterSpacing: 0,
+  },
+
+  DM_Bold_14_24_UC: {
+    fontSize: '14px',
+    fontWeight: 700,
+    lineHeight: '24px',
+    letterSpacing: '0.03em',
+    textTransform: 'uppercase',
+  },
+
+  DM_Med_14_24: {
+    fontSize: '14px',
+    fontWeight: 500,
+    lineHeight: '24px',
+    letterSpacing: 0,
+  },
+} as const;
+
+export default typography;
