7ef992e0c20b7bdbdd0b07ddd6c633ac69d61427
Przemysław Stochel <przemyslaw.stochel@revolve.pro>
2024-06-03 09:19:34 +0200

    YLH-1639 media js files added

---
+++ lib/styles/_media.d.ts
@@ -0,0 +1,3 @@
+export declare const laptopWidth = 1280;
+export declare const tabletWidth = 1024;
+export declare const verticalTabletWidth = 850;
---
+++ lib/styles/_media.ts
@@ -0,0 +1,3 @@
+export const laptopWidth = 1280;
+export const tabletWidth = 1024;
+export const verticalTabletWidth = 850;
