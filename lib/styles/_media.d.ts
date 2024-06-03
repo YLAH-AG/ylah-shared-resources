f589d849ea08af387ddce01a977167c84c8617f3
Przemysław Stochel <przemyslaw.stochel@revolve.pro>
2024-06-03 09:26:30 +0200

    YLH-1639 media enum added

--- lib/styles/_media.d.ts
+++ lib/styles/_media.d.ts
@@ -1,3 +1,9 @@
+export declare enum MEDIA {
+    DESKTOP = "DESKTOP",
+    LAPTOP = "LAPTOP",
+    TABLET = "TABLET",
+    TABLET_VERTICAL = "TABLET_VERTICAL"
+}
 export declare const laptopWidth = 1280;
 export declare const tabletWidth = 1024;
 export declare const verticalTabletWidth = 850;
--- lib/styles/_media.ts
+++ lib/styles/_media.ts
@@ -1,3 +1,10 @@
+export enum MEDIA {
+  DESKTOP = 'DESKTOP',
+  LAPTOP = 'LAPTOP',
+  TABLET = 'TABLET',
+  TABLET_VERTICAL = 'TABLET_VERTICAL',
+}
+
 export const laptopWidth = 1280;
 export const tabletWidth = 1024;
 export const verticalTabletWidth = 850;
