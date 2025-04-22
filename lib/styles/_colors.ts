0b6648ffe24993d9486164052ddd6ff36758051a
Anna Domańska <anna.domanska@revolve.pro>
2025-04-22 14:58:20 +0200

    feat: remove old branding & add missing variant with brand bolder

--- lib/styles/_colors.ts
+++ lib/styles/_colors.ts
@@ -1,67 +1,3 @@
-// TODO: remove old branding when all subtasks from YLH-3524 are done
-const oldBranding = {
-  // Primary & Secondary Colors
-  blue100: "#0D546E",
-  blue90: "#2E6780",
-  blue80: "#4c788f",
-  blue70: "#62899d",
-  blue60: "#799aab",
-  blue50: "#8faab9",
-  blue40: "#a5bbc7",
-  blue30: "#bcccd5",
-  blue20: "#d2dde3",
-  blue10: "#e9eef1",
-  blue7: "#f4f7f8",
-  blueExtraDark: "#01293A",
-  blueChipDarkBlue: "#004157",
-
-  mintMain: "#E9EEF1",
-  mintBackground: "#F9FBFB",
-  mintMedium: "#F4F7F8",
-  mintVeryLight: "#F9FBFB",
-
-  // Semantic Color
-  semanticOrange: "#FFC857",
-  semanticOrangeLight: "#FFF4DD",
-  semanticRed: "#E5323B",
-  semanticRedLight: "#FEF5F5",
-  semanticRedDarkText: "#A21A21",
-  semanticExtraDarkRed: "#891E23",
-
-  semanticGreenLight: "#E6ECE3",
-  semanticGreen: "#349E02",
-  semanticGreenDark: "#2B621B",
-
-  // Achromatic
-  achromaticOffWhite: "#FEFEFE",
-  achromaticOffBlack: "#1C1D1D",
-
-  achromaticGrey100: "#4F4F4F",
-  achromaticGrey80: "#828282",
-  achromaticGrey60: "#D0D1D2",
-  achromaticGrey50: "#E0E0E0",
-  achromaticGrey30: "#EBEBEB",
-  achromaticGrey20: "#F2F2F2",
-  achromaticGrey10: "#FAFBFC",
-
-  // Off-grays
-  offGraysDark: "#4C5657",
-  offGraysMediumWcag: "#4E595A",
-  offGraysNavigation: "#5C7379",
-  offGraysLightIcons: "#809294",
-
-  // Evaluation
-  evaluationMainRed8: "#c47472",
-  evaluationMainPurple6: "#968491",
-  evaluationLightBlue: "#CEDBF2",
-  evaluationMainBlue4: "#778c9d",
-  evaluationlightGreen: "#d8e7b9",
-  evaluationMainGreen2: "#adcc9c",
-
-  // Outline
-  outlineOrange: "#AD883B",
-};
-
 const baseColors = {
   white: "#FFF",
   black: "#000",
@@ -193,6 +129,7 @@ const colorCodes = {
 const colorAliases = {
   // Brand
   colorBrandBoldest: colorCodes.petrol1200,
+  colorBrandBolder: colorCodes.petrol1100,
   colorBrandBold: colorCodes.petrol1000,
 
   // Accent
@@ -202,6 +139,7 @@ const colorAliases = {
 
   // Text
   colorTextBrandBoldest: colorCodes.petrol1200,
+  colorTextBrandBolder: colorCodes.petrol1100,
   colorTextBrandBold: colorCodes.petrol1000,
   colorTextBoldest: colorCodes.achromatic1300,
   colorTextBold: colorCodes.tinted1100,
@@ -270,7 +208,6 @@ const colorAliases = {
 
 const colors = {
   ...baseColors,
-  ...oldBranding,
   ...colorCodes,
   ...colorAliases,
 } as const;
